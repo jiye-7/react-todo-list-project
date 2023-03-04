b028e7174786f0742eb40abb13090e6d9194eba9
jiye-7 <jy522hi@gmail.com>
2023-03-05 01:36:35 +0900

    feat: css 초기화, body에 기본 스타일 적용

--- src/App.js
+++ src/App.js
@@ -3,11 +3,8 @@ import './App.css';
 function App() {
   return (
     <div className='App'>
-      <header className='App-header'>
-        <p>
-          hoho ㅎㅎ test Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
+      <p>헤더</p>
+      <p>메인</p>
     </div>
   );
 }
