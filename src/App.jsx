2bf6efcf768e3032c69b561b062a9997ab7d0a83
jiye-7 <jy522hi@gmail.com>
2023-03-07 00:09:47 +0900

    feat: header layout

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Header from './components/Header/Header';
+import Main from './components/Main/Main';
+import styles from './App.module.css';
+
+function App() {
+  return (
+    <div className={styles.app}>
+      <Header />
+      <Main />
+    </div>
+  );
+}
+
+export default App;
