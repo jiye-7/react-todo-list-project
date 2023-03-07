cf2c89ffdc3b68e720efbb422afa40e6ba85df6b
jiye-7 <jy522hi@gmail.com>
2023-03-08 01:07:53 +0900

    feat: todo list 보여주기

--- src/components/Todo/TodoItem/TodoItem.jsx
+++ src/components/Todo/TodoItem/TodoItem.jsx
@@ -2,12 +2,16 @@ import React from 'react';
 import { HiOutlineTrash } from 'react-icons/hi';
 import styles from './TodoItem.module.css';
 
-export default function TodoItem({ todo }) {
+export default function TodoItem({ todo: { id, content, status } }) {
   return (
     <li className={styles.container}>
       <div className={styles.left_item}>
-        <input type='checkbox' id={todo.id} />
-        <label htmlFor={todo.id}>{todo.title}</label>
+        <input
+          type='checkbox'
+          id={id}
+          defaultChecked={status === 'completed'}
+        />
+        <label htmlFor={id}>{content}</label>
       </div>
       <div className={styles.right_item}>
         <HiOutlineTrash />
